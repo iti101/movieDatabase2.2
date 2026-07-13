4f4375ab0a94b3aef7853401ea112265bbfa2ee7
itamar <itamar.turgman@gmail.com>
2026-07-13 18:01:30 +0200

    adding basic NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import Navbar from './components/Navbar';
 
 function App() {
     return (
         <>
             <LandingPage/>
+            <Navbar/>
         </>
     );
 }
