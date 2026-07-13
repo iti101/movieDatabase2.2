732d0432da64221993f185b4b2652b3a6cc576f1
itamar <itamar.turgman@gmail.com>
2026-07-13 17:33:42 +0200

    creating basic page structure

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="container">
+      <div className="section section1">
+        <h1>Landing Section</h1>
+      </div>
+      <div className="section section2">
+        <h1>Search Section</h1>
+      </div>
+      <div className="section section3">
+        <h1>Suggest Section</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
